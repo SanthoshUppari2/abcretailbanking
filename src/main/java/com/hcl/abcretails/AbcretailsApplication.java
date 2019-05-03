72b8bc94db444634bc4719adfff98a80d8dc9ce4
IngTraining <joydeep.j@hcl.com>
2019-05-03 11:12:57 +0530

    initial setup files

---
+++ src/main/java/com/hcl/abcretails/AbcretailsApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.abcretails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcretailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcretailsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hcl/abcretails/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.hcl.abcretails.controller;
+
+
+public class UserController {
+	
+	public void test(){
+		System.out.println("");
+	}
+
+}
